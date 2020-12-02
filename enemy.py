d9c2cd394e63eda794e03c066e8355052d3b8c31
Ben Chan <benchan777@gmail.com>
2020-12-02 11:53:57 -0800

    add attack method

--- enemy.py
+++ enemy.py
@@ -14,3 +14,9 @@ class Enemy:
         # enemy loot, modified by level
         self.gold = random.randint(30, 50) * level
         self.potions = random.randint(0, 3) * level
+
+    def attack(self):
+        ''' Calculates damage done by player to opponent '''
+        attack_damage = random.randint(1, 6)
+        # print(f"You deal {attack_damage} damage to your opponent!")
+        return attack_damage
