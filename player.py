8498e43824779df16aa5a808d528675707d0fc6c
Ben Chan <benchan777@gmail.com>
2020-12-01 17:48:24 -0800

    add attack function

--- player.py
+++ player.py
@@ -1,9 +1,16 @@
+import random
+
 class Player:
     def __init__(self, name):
         self.hp = 30
+        self.armor = 0
         self.name = name
 
         # inventory
         self.potions = 3
         self.revives = 0
         self.gold = 0
+
+    def attack(self):
+        attack_damage = random.randint(1, 6)
+        return attack_damage
