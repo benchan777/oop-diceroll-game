a0c922b0d385947c1cbea4ff847c94bef55579cc
Ben Chan <benchan777@gmail.com>
2020-12-01 20:19:57 -0800

    edit enemy class

--- characters.py
+++ characters.py
@@ -1,32 +1,10 @@
 import random
 
-class Player:
-    def __init__(self, name):
-        ''' Instantiates the player class '''
-        self.hp = 30
-        self.armor = 0
-        self.name = name
-
-        # inventory
-        self.potions = 3
-        self.revives = 0
-        self.gold = 0
-
-    # player attack function that deals random damage between 1 and 6
-    def attack(self):
-        attack_damage = random.randint(1, 6)
-        return attack_damage
-
 class Enemy:
-    def __init__(self, level):
-        self.hp = 30 * level
+    def __init__(self, level, hp = 30):
+        self.hp = hp * level
         self.name = "Enemy"
 
         # enemy loot, modified by level
         self.gold = random.randint(30, 50) * level
         self.potions = random.randint(0, 1) * level
-
-    # enemy attack function that deals random damage between 1 and 6
-    def attack(self):
-        attack_damage = random.randint(1, 6)
-        return attack_damage
