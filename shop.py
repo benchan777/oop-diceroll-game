d880b970f60d6a1d166c87d3f0dbf9d612d47a44
Ben Chan <benchan777@gmail.com>
2020-12-03 02:50:23 -0800

    why am i still working on this

--- shop.py
+++ shop.py
@@ -1,5 +1,9 @@
+from player import Player
+
 class Shop:
-    gold = 0
+    potion_price = 3
+    armor_price = 10
+    revives_price = 100
 
     def __init__(self, potions = 100, armor = 100, revives = 20):
         ''' Instantiates the Shop class with the following properties:
@@ -13,4 +17,15 @@ class Shop:
 
     def display_shop(self):
         ''' Displays current shop inventory '''
-        print(f"Potions: {self.potions} Price: 3 gold\nArmor: {self.armor} Price: 10 gold\nRevives: {self.revives} Price: 100 gold")
+        print(f"1 - Potions: {self.potions} Price: 3 gold\n2 - Armor: {self.armor} Price: 10 gold\n3 - Revives: {self.revives} Price: 100 gold\n")
+        shop_input = int(input(f"What would you like to purchase? "))
+
+        while shop_input < 4:
+            if shop_input == 1:
+                if Player.gold < Shop.potion_price:
+                    print("You have insufficient gold!")
+                else:
+                    self.potions -= 1
+                    Player.gold - Shop.potion_price
+                    print(f"You have purchased 1 potion! You have {Player.gold} gold remaining.")
+        print("exiting shop\n")
