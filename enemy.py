a20bb7ff9678611094013b7a1169d15136265f6c
Ben Chan <benchan777@gmail.com>
2020-12-02 23:25:30 -0800

    add get_name and change_level methods

--- enemy.py
+++ enemy.py
@@ -1,28 +1,40 @@
 import random
 
 class Enemy:
-    def __init__(self, level, hp = 30):
+    level = 1
+
+    def __init__(self, name, hp = 30):
         ''' Instance properties:
             hp: int
             name: string
             gold: int
             potions: int
         '''
-        self.hp = hp * level
-        self.name = "Enemy"
+        self.hp = hp * Enemy.level
+        self.name = name
 
         # enemy loot, modified by level
-        self.gold = random.randint(30, 50) * level
-        self.potions = random.randint(0, 3) * level
+        self.gold = random.randint(30, 50) * Enemy.level
+        self.potions = random.randint(0, 3) * Enemy.level
 
     def attack(self):
         ''' Calculates damage done by opponent to player '''
         attack_damage = random.randint(1, 6)
         return attack_damage
 
-    def revive(self, level, hp = 30):
+    def revive(self, hp = 30):
         ''' Revives the enemy for the next round with newly generated loot '''
-        self.hp = hp * level
-        self.gold = random.randint(30, 50) * level
-        self.potions = random.randint(0, 3) * level
+        self.hp = hp * Enemy.level
+        self.gold = random.randint(30, 50) * Enemy.level
+        self.potions = random.randint(0, 3) * Enemy.level
         print(f"hp:{self.hp}gold:{self.gold}potions:{self.potions}")
+
+    def change_level(self):
+        ''' Changes the level of the enemy '''
+        level_input = int(input("Enter the level of the enemy you'd like to fight: "))
+        Enemy.level = level_input
+
+    def get_name(self):
+        ''' Retrieves the enemy's name '''
+        name = self.name
+        return name
